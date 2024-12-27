0d79fba3981f4c41ba7c7ae999708d5f55de1924
siddharth-techie-vyas <sid.vyas786@gmail.com>
2024-12-27 15:47:15 +0530

    model with

--- web/footer.php
+++ web/footer.php
@@ -137,7 +137,23 @@ $('.select2').select2();
 </div>
 
 <!-- Modal -->
+<style>
+  body .modal { /* Width */
+    max-width: 100%;
+    width: auto !important;
+    display: inline-block;
+}
+.modal {
+  z-index: -1;
+  display: flex !important;
+  justify-content: center;
+  align-items: center;
+}
 
+.modal {
+   z-index: 1050;
+}
+</style>
 <div id="myModal2" class="modal fade" role="dialog">
   <div class="modal-dialog">
     <!-- Modal content-->
