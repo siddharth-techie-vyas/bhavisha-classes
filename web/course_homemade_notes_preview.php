1b36f9363360593dd6aa90162a810722e6cfab74
siddharth-techie-vyas <sid.vyas786@gmail.com>
2024-12-28 14:52:03 +0530

    div

--- web/course_homemade_notes_preview.php
+++ web/course_homemade_notes_preview.php
@@ -14,8 +14,8 @@ $get_details = $course->get_one_handmade_notes($_GET['id']);
 
 <div id="pdf_editor"></div>
 
-<div id="canvas_div_pdf" style="width:100%; padding:2px; border:1px solid #000; text-align:justyify;">
-
+<div id="canvas_div_pdf" >
+<div style="width:100%; padding:2px; border:1px solid #000; text-align:justyify;">
                                             
                                             <table border="0" valign="top" style="width:100%;">
                                                 <thead>
@@ -59,7 +59,7 @@ $get_details = $course->get_one_handmade_notes($_GET['id']);
                                             <?php }?>
                                             
 </div>
-
+</div>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.8.0/html2pdf.bundle.min.js"></script>
 <script>
